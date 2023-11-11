cf2d17c2a9d8badacaff1542f44adfc8d8f85dcf
pushkarchand <pushkarbc.11ec@gmail.com>
2023-11-11 13:11:42 +0530

    Added the editorjs and backend code

--- frontend/src/modules/myoverview/views/index.tsx
+++ frontend/src/modules/myoverview/views/index.tsx
@@ -1,6 +1,29 @@
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import AuthContext from '@context/authprovider';
+import ReichTextEditor from '@modules/common/components/richtexteditor';
+import MainNavigation from '@modules/common/components/mainnavigation';
+import SideBar from '@modules/common/components/sidebar';
+import styled from '@emotion/styled';
+import spacing from '@utils/styles/spacing';
+
+const MainContainer = styled.div`
+  display: flex;
+  box-sizing: borde-box;
+`;
+
+const RightSection = styled.div`
+  display: flex;
+  width: 150px;
+`;
+
+const CenterSection = styled.div`
+  display: flex;
+  flex-grow: 1;
+  padding: ${spacing.small}px;
+  box-sizing: borde-box;
+  padding: ${spacing.large}px;
+`;
 
 const Home = () => {
   const { setAuth }: any = useContext(AuthContext);
@@ -13,11 +36,14 @@ const Home = () => {
 
   return (
     <section>
-      <h1>Home</h1>
-      <p>You are logged in!</p>
-      <div className="flexGrow">
-        <button onClick={logout}>Sign Out</button>
-      </div>
+      <MainNavigation />
+      <MainContainer>
+        <SideBar />
+        <CenterSection>
+          <ReichTextEditor />
+        </CenterSection>
+        <RightSection>- First</RightSection>
+      </MainContainer>
     </section>
   );
 };
