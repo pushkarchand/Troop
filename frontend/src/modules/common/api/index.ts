f859ac08e75325eeb3e9e492715cec7e483a0bd7
pushkarchand <pushkarbc.11ec@gmail.com>
2023-12-16 08:11:25 +0530

    Replaced the upload endpoint with environment API endpoint

--- frontend/src/modules/common/api/index.ts
+++ frontend/src/modules/common/api/index.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
+const baseUrl = import.meta.env.VITE_REACT_APP_BASE_URL;
 export const upload = async (file: any) => {
   const formData = new FormData();
   formData.append('image', file);
 
-  return await axios.post('http://localhost:5001/upload', formData, {
+  return await axios.post(`${baseUrl}/upload`, formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
       Authorization: `Bearer ${localStorage.getItem('alluvium_auth_token')}`,
